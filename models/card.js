7fcd84673550818eb82e915be9c6f28e9d51914b
Aleksey Tatarov <alekseytatarv@gmail.com>
2022-10-21 20:48:34 +0400

    Add card and user models. Closed #3

---
+++ models/card.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minLength: 2,
+    maxLengthL: 30,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "user",
+  },
+  likes: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "user",
+      default: [],
+    },
+  ],
+  createdAt: {
+    type: Number,
+    default: Date.now().getTime(),
+  },
+});
+
+module.exports = mongoose.model("card", cardSchema);
