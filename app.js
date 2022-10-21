d590b0a76c4f907bb60738741365492bcd518b41
Aleksey Tatarov <alekseytatarv@gmail.com>
2022-10-21 20:33:18 +0400

    Add mongoose

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-const express = require('express');
+const express = require("express");
+const mongoose = requure("mongoose");
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+const URI = "mongodb://localhost:27017/prostogramdb";
+
+mongoose.connect(
+  URI,
+  { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false },
+  (error) => {
+    if (error) throw error.message;
+
+    console.log(`Connected to ${URI}`);
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
